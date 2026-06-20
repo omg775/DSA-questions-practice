b80fe180408187cd93198789a4172e05f14a390f
om gawde <omgawde775@gmail.com>
2026-06-20 23:47:58 +0200

    Insertion Sort

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,7 +1,7 @@
 public class InsertionSort {
     public static void main(String[] args) {
 
-        int arr[] = {5,6,2,3,1};
+        int arr[] = {9,6,2,3,1};
 
         for(int i=1;i<arr.length;i++){
             int key = arr[i];
