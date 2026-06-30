d47baf68c5302930e3adc73f7b8712a3760bfa11
om gawde <omgawde775@gmail.com>
2026-06-30 05:19:56 +0200

    Linked list

---
+++ src/linked/LinkedList.java
@@ -0,0 +1,38 @@
+package linked;
+
+class Node{
+    int data;
+    Node next;
+
+    public Node(int data){
+        this.data = data;
+        next = null;
+    }
+}
+
+public class LinkedList {
+    Node head = null;
+
+    public void add(int data){
+        Node newNode = new Node(data);
+
+        Node current = head;
+        if(head == null)
+            head = newNode;
+        else{
+            while(current.next != null){
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+
+    }
+    public void printValues(){
+        Node current = head;
+
+        while(current != null){
+            System.out.println(current.data + " ");
+            current = current.next;
+        }
+    }
+}
---
+++ src/linked/Main.java
@@ -0,0 +1,18 @@
+package linked;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList nums = new LinkedList();
+
+        nums.add(5);
+        nums.add(9);
+
+        nums.add(6);
+//        System.out.println(nums.get(2));
+//        nums.peek();
+
+        nums.printValues();
+
+    }
+}
